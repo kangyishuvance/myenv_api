26c7b73fe45d4878ba426c2037d06f2c7d6e773c
kangyishuvance <kangyishuvance@gmail.com>
2025-01-11 22:16:49 +0800

    change1

--- import requests.py
+++ import requests.py
@@ -4,6 +4,16 @@ import pandas as pd
 import geocoder
 from geopy.distance import geodesic
 
+import tkinter as tk
+from tkinter import messagebox
+
+
+def show_dialogue_box(message):
+    root = tk.Tk()
+    root.withdraw()  # Hide the root window
+    messagebox.showinfo("Temperature Information", message)
+    root.destroy()
+
 def get_air_temperature(date=None, pagination_token=None):
     base_url = "https://api-open.data.gov.sg/v2/real-time/api/air-temperature"
     params = {}
@@ -36,6 +46,15 @@ current_coords = g.latlng
 #%%
 
 
+
+        
+
+
+time = data['data']['readings'][0]["timestamp"].split("+")[0][:16]
+
+df_current = pd.DataFrame(data['data']['readings'][0]["data"])
+
+
 df_stations = pd.DataFrame(data["data"]["stations"])
 
 
@@ -45,12 +64,13 @@ df_stations = df_stations.drop(columns=['location']).join(location_df)
 df_stations['distance'] = df_stations.apply(
         lambda row: geodesic(current_coords, (row['latitude'], row['longitude'])).meters, axis=1
     )
-closest_station_id = df_stations.loc[df_stations['distance'].idxmin()]['id']
-        
 
+df_stations_available = df_stations[df_stations['id'].isin(df_current.stationId.unique())]
+closest_station_id = df_stations_available.loc[df_stations_available['distance'].idxmin()]['id']
 
-time = data['data']['readings'][-1]["timestamp"]
+station_name= df_stations_available.loc[df_stations_available.id==closest_station_id]['name'].values[0]
 
-df_current = pd.DataFrame(data['data']['readings'][-1]["data"])
+message = f"The tempearture is {df_current[df_current['stationId']==closest_station_id]['value'].values[0]} at time of {time} at closest station available {station_name}"
+print(message)
+show_dialogue_box(message)
 
-print(f"The tempearture is {df_current[df_current['stationId']==closest_station_id]['value'].values[0]} at time of {time}")
